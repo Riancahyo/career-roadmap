ed2be58eb717131b6958f76c58b66c9ed3a5befd
Riancahyo <riancahyo75@gmail.com>
2026-01-08 15:25:03 +0700

    Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,31 +4,33 @@ import LinkedInIcon from '../assets/icons/linkedin.svg'
 export const Footer = () => {
   return(
     <footer className='py-5 bg-black text-white/60 border-t border-white/20'>
-      <div className="container">
-        <div className='flex flex-col gap-5 sm:flex-row sm:justify-between'>
-          <div className="text-center">© 2026 • Rian Cahyo</div>
-          <ul className='flex justify-center gap-2.5'>
-            <li>
-              <a 
-                href="https://www.linkedin.com/in/riancahyoanggoro/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <LinkedInIcon/>
-              </a>
-            </li>
-            <li>
-              <a 
-                href="https://www.instagram.com/rianchyoa" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <InstaIcon/>
-              </a>
-            </li>
-          </ul>
+      <div className="px-4">
+        <div className="container">
+          <div className='flex flex-col gap-5 sm:flex-row sm:justify-between'>
+            <div className="text-center">© 2026 • Rian Cahyo</div>
+            <ul className='flex justify-center gap-2.5'>
+              <li>
+                <a 
+                  href="https://www.linkedin.com/in/riancahyoanggoro/" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80 transition-opacity"
+                >
+                  <LinkedInIcon/>
+                </a>
+              </li>
+              <li>
+                <a 
+                  href="https://www.instagram.com/rianchyoa" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80 transition-opacity"
+                >
+                  <InstaIcon/>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
